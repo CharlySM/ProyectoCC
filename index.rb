19a9baf040126a4c3ba8449075517ce29bf958d9
CharlySM <csm89velocista@gmail.com>
2021-11-05 22:22:42 +0100

    prueba 3

--- index.rb
+++ index.rb
@@ -6,21 +6,6 @@ require './src/User'
 require './src/Utils'
 require './src/Equipo'
 require 'logger'
-require 'etcdv3'
-
-::Logger.class_eval { alias :write :'<<' }
-  access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','access.log')
-  access_logger = ::Logger.new(access_log)
-  error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','error.log'),"a+")
-  error_logger.sync = true
-
-  configure do
-    use ::Rack::CommonLogger, access_logger
-  end
-
-  before {
-    env["rack.errors"] =  error_logger
-  }
 
 set :bind, "0.0.0.0"
 set :port, 80
@@ -30,8 +15,6 @@ collection = client["users"]
 request = client["request"]
 @data=" "
 $user=
-conn = Etcdv3.new(endpoints: 'http://127.0.0.1:2379')
-puts "port: #{conn.get("SinatraPort")}"
 
 Utils=Utils.instance
 get '/' do
